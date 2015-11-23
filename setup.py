984f3185f9afdec15597ea90e23fbc257877cabd
Hilton Campbell <hiltonc@ldschurch.org>
2015-11-22 21:12:06 -0700

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-grappelli-autocomplete-fk-edit-link',
-    version='0.1.0',
+    version='0.1.1',
     packages=['grappelli_autocomplete_fk_edit_link',],
     include_package_data=True,
     license='MIT',
