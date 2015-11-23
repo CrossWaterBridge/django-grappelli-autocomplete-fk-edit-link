9d248f1a0de57c9beec5ea70509552e7d2d23dbe
Hilton Campbell <hiltonc@ldschurch.org>
2015-11-22 20:44:53 -0700

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-grappelli-autocomplete-fk-edit-link',
-    version='1.0.0dev',
+    version='0.1.0',
     packages=['grappelli_autocomplete_fk_edit_link',],
     include_package_data=True,
     license='MIT',
     description='ModelAdmin mixin that adds edit links to Django Grappelli autocomplete lookups.',
-    long_description=open('README.md').read(),
+    url='https://github.com/CrossWaterBridge/django-grappelli-autocomplete-fk-edit-link',
+    download_url='https://github.com/CrossWaterBridge/django-grappelli-autocomplete-fk-edit-link/tarball/0.1.0',
     install_requires=[
         'django-grappelli>=2.6.3',
     ],
